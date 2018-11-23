66007be20f2199d0148fb69f21647a0f0940274a
willpile <willpile@users.noreply.github.com>
2018-11-23 15:11:28 -0600

    Update relays-test.py

--- Relay/relays-test.py
+++ Relay/relays-test.py
@@ -2,6 +2,7 @@
 
 import sys
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setwarnings(False)
 
@@ -25,52 +26,156 @@ GPIO.setup(16, GPIO.OUT) # relay 14
 GPIO.setup(20, GPIO.OUT) # relay 15
 GPIO.setup(21, GPIO.OUT) # relay 16
 
+#initialize with All relays off
+print('initialize all relays off')
 
-if sys.argv[1] == '1':
-	output_pin = 4
-if sys.argv[1] == '2':
-	output_pin = 17
-if sys.argv[1] == '3':
-	output_pin = 27
-if sys.argv[1] == '4':
-	output_pin = 22
-if sys.argv[1] == '5':
-	output_pin = 5
-if sys.argv[1] == '6':
-	output_pin = 6
-if sys.argv[1] == '7':
-	output_pin = 13
-if sys.argv[1] == '8':
-	output_pin = 19
-if sys.argv[1] == '9':
-	output_pin = 26
-if sys.argv[1] == '10':
-	output_pin = 23
-if sys.argv[1] == '11':
-	output_pin = 24
-if sys.argv[1] == '12':
-	output_pin = 25
-if sys.argv[1] == '13':
-	output_pin = 12
-if sys.argv[1] == '14':
-	output_pin = 16
-if sys.argv[1] == '15':
-	output_pin = 20
-if sys.argv[1] == '16':
-	output_pin = 21
+GPIO.output(4, 1)
+GPIO.output(17, 1)
+GPIO.output(27, 1)
+GPIO.output(22, 1)
+GPIO.output(5, 1)
+GPIO.output(6, 1)
+GPIO.output(13, 1)
+GPIO.output(19, 1)
+GPIO.output(26, 1)
+GPIO.output(23, 1)
+GPIO.output(24, 1)
+GPIO.output(25, 1)
+GPIO.output(12, 1)
+GPIO.output(16, 1)
+GPIO.output(20, 1)
+GPIO.output(21, 1)
 
+print(' relay 1 HI, waiting 1 second')
+time.sleep(1)
+GPIO.output(4, 0)
 
+print('relay 1 LOW, waiting 1 second')
+time.sleep(1)
+GPIO.output(4, 1)
 
+print('relay 2 HI, waiting 1 second')
+time.sleep(1)
+GPIO.output(17, 0)
 
+print('relay 2 LOW, waiting 1 second')
+time.sleep(1)
+GPIO.output(17, 1)
 
-if sys.argv[2] == 'on':
-	output_relay = 0 # because it's backwards binary
-else:
-	if sys.argv[2] == 'off':
-		output_relay = 1 # because it's backwards binary
-	else:
-		print "error"
+print('relay 3 LOW, waiting 1 second')
+time.sleep(1)
+GPIO.output(27, 0)
 
-GPIO.output(output_pin, output_relay)
+print('relay 3 HI, waiting 1 second')
+time.sleep(1)
+GPIO.output(27, 1)
+
+print('relay 4 LOW, waiting 1 second')
+time.sleep(1)
+GPIO.output(22, 0)
+
+print('relay 4 HI, waiting 1 second')
+time.sleep(1)
+GPIO.output(22, 1)
+
+print('relay 5 LOW, waiting 1 second')
+time.sleep(1)
+GPIO.output(5, 0)
+
+print('relay 5 HI, waiting 1 second')
+time.sleep(1)
+GPIO.output(5, 1)
+
+print('relay 6 LOW, waiting 1 second')
+time.sleep(1)
+GPIO.output(6, 0)
+
+print('relay 6 HI, waiting 1 second')
+time.sleep(1)
+GPIO.output(6, 1)
+
+print('relay 7 LOW, waiting 1 second')
+time.sleep(1)
+GPIO.output(13, 0)
+
+print('relay 7 HI, waiting 1 second')
+time.sleep(1)
+GPIO.output(13, 1)
+
+print('relay 8 LOW, waiting 1 second')
+time.sleep(1)
+GPIO.output(19, 0)
+
+print('relay 8 HI, waiting 1 second')
+time.sleep(1)
+GPIO.output(19, 1)
+
+print('relay 9 LOW, waiting 1 second')
+time.sleep(1)
+GPIO.output(26, 0)
+
+print('relay 9 HI, waiting 1 second')
+time.sleep(1)
+GPIO.output(26, 1)
+
+print('relay 10 LOW, waiting 1 second')
+time.sleep(1)
+GPIO.output(23, 0)
+
+print('relay 10 HI, waiting 1 second')
+time.sleep(1)
+GPIO.output(23, 1)
+
+print('relay 11 LOW, waiting 1 second')
+time.sleep(1)
+GPIO.output(24, 0)
+
+print('relay 11 HI, waiting 1 second')
+time.sleep(1)
+GPIO.output(24, 1)
+
+print('relay 12 LOW, waiting 1 second')
+time.sleep(1)
+GPIO.output(25, 0)
+
+print('relay 12 HI, waiting 1 second')
+time.sleep(1)
+GPIO.output(25, 1)
+
+print('relay 13 LOW, waiting 1 second')
+time.sleep(1)
+GPIO.output(12, 0)
+
+print('relay 13 HI, waiting 1 second')
+time.sleep(1)
+GPIO.output(12, 1)
+
+print('relay 14 LOW, waiting 1 second')
+time.sleep(1)
+GPIO.output(16, 0)
+
+print('relay 14 HI, waiting 1 second')
+time.sleep(1)
+GPIO.output(16, 1)
+
+print('relay 15 LOW, waiting 1 second')
+time.sleep(1)
+GPIO.output(20, 0)
+
+print('relay 15 HI, waiting 1 second')
+time.sleep(1)
+GPIO.output(20, 1)
+
+print('relay 16 LOW, waiting 1 second')
+time.sleep(1)
+GPIO.output(21, 0)
+
+print('relay 16 HI, waiting 1 second')
+time.sleep(1)
+GPIO.output(21, 1)
+
+GPIO.cleanup()
+print('cleaned up')
+
 
 #EOF
