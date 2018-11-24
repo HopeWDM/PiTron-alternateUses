e833d8fd4c88090460598309e8986aa3ccd9e3c3
willpile <willpile@users.noreply.github.com>
2018-11-24 08:47:09 -0600

    added notes

--- Relay/relay_1_on.py
+++ Relay/relay_1_on.py
@@ -9,12 +9,14 @@ import RPi.GPIO as GPIO
 GPIO.setwarnings(False)
 
 # using GPIO 4 as the relay 1 output
+# chan_list not necessary but helpful if multiple channels are intialized
 
 chan_list = [4]
  
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(chan_list, GPIO.OUT)
 
+# GPIO.output(channel, 0 = on 1 = off)
 GPIO.output(4, 0)
 
 #EOF
